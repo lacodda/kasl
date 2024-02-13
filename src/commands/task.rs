a15e76a8e899dbc4c5bd7853ff5108b9d3f98dfc
Kirill Lakhtachev <lahtachev@gmail.com>
2024-02-13 17:14:02 +0100

    feat: Task structure, tasks scheme and "insert_task" method has been added

--- src/commands/task.rs
+++ src/commands/task.rs
@@ -1,5 +1,27 @@
+use crate::libs::db::Db;
 use clap::Args;
 use std::error::Error;
+
+#[derive(Debug)]
+pub struct Task {
+    pub id: Option<i32>,
+    pub timestamp: Option<String>,
+    pub name: String,
+    pub comment: String,
+    pub completeness: i32,
+}
+
+impl Task {
+    fn new(name: &str, comment: &str, completeness: &i32) -> Self {
+        Task {
+            id: None,
+            timestamp: None,
+            name: name.to_string(),
+            comment: comment.to_string(),
+            completeness: completeness.to_owned(),
+        }
+    }
+}
 
 #[derive(Debug, Args)]
 pub struct TaskArgs {
@@ -8,6 +30,9 @@ pub struct TaskArgs {
 }
 
 pub fn cmd(task_args: TaskArgs) -> Result<(), Box<dyn Error>> {
+    let task = Task::new(&task_args.name, "", &10);
+    let _ = Db::new()?.insert_task(&task);
+
     println!("Task {}", &task_args.name);
 
     Ok(())
