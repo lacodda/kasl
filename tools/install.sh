e0ac27cfbaca539181155811d2f5ab74fba4c3be
Kirill Lakhtachev <lahtachev@gmail.com>
2024-03-19 11:07:33 +0100

    feat: Bash installation script has been slightly modified

--- tools/install.sh
+++ tools/install.sh
@@ -8,25 +8,28 @@ case "$OS" in
   *) echo "Unsupported OS: $OS"; exit 1;;
 esac
 
-REPO_USER="lacodda"
-REPO_NAME="kasl"
-LATEST_VERSION=$(curl -s https://api.github.com/repos/$REPO_USER/$REPO_NAME/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+VENDOR="lacodda"
+APP_NAME="kasl"
+LATEST_VERSION=$(curl -s https://api.github.com/repos/$VENDOR/$APP_NAME/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 if [ -z "$LATEST_VERSION" ]; then
     echo "Failed to fetch the latest version"
     exit 1
 fi
 
-RELEASE_URL="https://github.com/$REPO_USER/$REPO_NAME/releases/latest/download/$REPO_NAME-$LATEST_VERSION-x86_64-$OS.tar.gz"
+RELEASE_URL="https://github.com/$VENDOR/$APP_NAME/releases/latest/download/$APP_NAME-$LATEST_VERSION-x86_64-$OS.tar.gz"
 
 if [ "$OS" = "linux" ] || [ "$OS" = "darwin" ]; then
   DESTINATION="$HOME/.local/bin"
+  FULL_PATH="$DESTINATION/$APP_NAME"
 else  # windows
-  DESTINATION="$USERPROFILE/AppData/Local/Programs/$REPO_USER/$REPO_NAME"
+  DESTINATION="$USERPROFILE/AppData/Local/Programs/$VENDOR/$APP_NAME"
+  FULL_PATH="$DESTINATION/$APP_NAME.exe"
 fi
 
 echo "Downloading and unpacking $RELEASE_URL to $DESTINATION..."
 mkdir -p "$DESTINATION"
 curl -L "$RELEASE_URL" | tar -xz -C "$DESTINATION" --strip-components=1
+"$FULL_PATH" init
 
-echo "🎉 Congratulations! $REPO_NAME $LATEST_VERSION was successfully installed."
+echo "🎉 Congratulations! $APP_NAME $LATEST_VERSION was successfully installed."
