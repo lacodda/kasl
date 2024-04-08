6bfe59c83f25dc4f04cfc7db8ec1e4dfb2853426
Kirill Lakhtachev <lahtachev@gmail.com>
2024-04-08 12:24:02 +0200

    feat: Added aliases for event command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod commands;
 use clap::{Parser, Subcommand};
-use commands::{event, init, report, sum, task};
+use commands::{event::{self, EventArgs}, init, report, sum, task};
+use libs::event::EventType;
 use std::error::Error;
 mod api;
 mod db;
@@ -22,6 +23,10 @@ enum Commands {
     Task(task::TaskArgs),
     #[command(about = "Write timestamp and event type to database", arg_required_else_help = true)]
     Event(event::EventArgs),
+    #[command(about = "Write start timestamp to database")]
+    Start,
+    #[command(about = "Write end timestamp to database")]
+    End,
     #[command(about = "Get summary")]
     Sum(sum::SumArgs),
     #[command(about = "Prepare a report")]
@@ -35,6 +40,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         Commands::Init(args) => init::cmd(args),
         Commands::Task(args) => task::cmd(args),
         Commands::Event(args) => event::cmd(args),
+        Commands::Start => event::cmd(EventArgs{ event_type: EventType::Start, show: false }),
+        Commands::End => event::cmd(EventArgs{ event_type: EventType::End, show: false }),
         Commands::Sum(args) => sum::cmd(args),
         Commands::Report(args) => report::cmd(args),
     }
